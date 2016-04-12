00fb5be83e324b65cc6509a4e80a15af1edc7e04
akihiro nakao <nakao@diveintocode.jp>
2016-04-12 07:34:21 +0000

    until confirm view, no include back button

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -36,7 +36,7 @@ class BlogsController < ApplicationController
   
   private
     def blogs_params
-      params.require(:blog).permit(:title, :content)
+      params.require(:blog).permit(:title, :content, :confirming)
     end
     
     def set_blog
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,12 @@
 class Blog < ActiveRecord::Base
   validates :title, presence: true
+  validates :confirming, presence: true
+
+  attr_accessor :confirming
+  after_validation :check_confirming
+
+  def check_confirming
+    errors.delete(:confirming)
+    self.confirming = errors.empty? ? '1' : ''
+  end
 end
