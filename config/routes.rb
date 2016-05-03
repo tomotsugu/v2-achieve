97e7bd0c0fd17c3696ca9804f7a3f09a57ea1884
nakao <genn777f3@gmail.com>
2016-05-03 02:47:04 +0000

    day9

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:show, :edit, :update]
+  
   resources :blogs, only: [:index, :new, :create, :edit, :update ,:destroy] do 
     collection do 
       post :confirm
